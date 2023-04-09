64c122790e2ec601ac724f6d306d74331a25102a
Russell Gill <76260172+Russell-Gill@users.noreply.github.com>
2023-04-09 12:37:35 +0000

    Delete run_model.py

--- run_model.py
+++
@@ -1,6 +0,0 @@
-from main import create_token_sequences
-from tensorflow import keras
-model = keras.models.load_model('model.h5')
-
-new_my_input_sequences = create_token_sequences(["You can always mess around with my project that I’m doing if you want to feel extra demoralised"], tokenizer, maxlen=max_words)
-print(model.predict(new_my_input_sequences))
